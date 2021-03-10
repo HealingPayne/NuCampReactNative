763a42c99feefb759506b64adfbc1defccd88169
HealingPayne <dellpayne@msn.com>
2021-03-09 18:29:41 -0700

    Using Redux

--- App.js
+++ App.js
@@ -1,23 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-//import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
 import Main from './components/MainComponent'
 
 export default function App() {
+  const store = ConfigureStore();
   return (
-    <Main />
-    // <View style={styles.container}>
-    //   <Text>Hello there Dell Payne!</Text>
-    //   <StatusBar style="auto" />
-    // </View>
+    <Provider store={store}>
+      <Main />
+    </Provider>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center'
-//   },
-// });
