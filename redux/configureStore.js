598eea1c240e0652374db32f40e704a9adff8003
HealingPayne <dellpayne@msn.com>
2021-03-18 20:50:49 -0600

    Persist Redux Store

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,15 +1,23 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/es/storage'; //Access to local storage
+
 import { campsites } from './campsites';
 import { comments } from './comments';
 import { promotions } from './promotions';
 import { partners } from './partners';
 import { favorites } from './favorites';
 
+const config = {
+    key: 'root',
+    storage,
+    debug: true
+}
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
+        persistCombineReducers(config, {
             campsites,
             comments,
             partners,
@@ -18,6 +26,6 @@ export const ConfigureStore = () => {
         }),
         applyMiddleware(thunk, logger)
     );
-
-    return store;
+    const persistor = persistStore(store);
+    return {persistor, store};
 }
