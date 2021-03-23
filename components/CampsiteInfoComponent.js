aca760b685bab1c1c0d912dc7889e38381f20da8
HealingPayne <dellpayne@msn.com>
2021-03-23 17:45:50 -0600

    Social Sharing

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import {
     Text, View, ScrollView, FlatList,
-    Modal, Button, StyleSheet, Alert, PanResponder
+    Modal, Button, StyleSheet, Alert, PanResponder,
+    Share
 } from 'react-native';
 import { Card, Icon, Rating, Input } from 'react-native-elements';
 // import { CAMPSITES } from '../shared/campsites';
@@ -69,6 +70,15 @@ function RenderCampsite(props) {
             return true;
         }
     });
+    const shareCampsite = (title, message, url) => {
+        Share.share({
+            title: title, //Not Required
+            message: `${title}: ${message} ${url}`,
+            url: url
+        }, {
+            dialogTitle: 'Share ' + title
+        });
+    };
     if (campsite) {
         return (
             <Animatable.View 
@@ -100,6 +110,14 @@ function RenderCampsite(props) {
                             raised
                             reverse
                             onPress={() => props.onShowModal()}
+                        />
+                        <Icon
+                            name={'share'}
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)}
                         />
                     </View>
                 </Card>
